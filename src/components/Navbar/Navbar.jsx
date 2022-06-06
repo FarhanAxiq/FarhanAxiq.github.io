ea926e42cd4cee4e307bd52cf8c93e8b0b164151
FarhanAxiq <angahmyone@aol.com>
2022-06-05 18:05:56 -0600

    navbar work, need rto fill up more

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import'./nav.css';
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 import {useState} from 'react';
 
 
@@ -10,10 +12,18 @@ const Navbar = () => {
     setNavbarOpen(prev => !prev)
   }
 
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  }
+
   return (
     <nav className="navBar">
-      <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
-      <ul>...</ul>
+      <button onClick={handleToggle}>
+        {navbarOpen ? ( <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} /> ) 
+        : (<FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+        )}
+      </button>
+      <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}></ul>
     </nav>
   )
 }
